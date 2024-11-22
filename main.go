a82ec733f5ca464d6557a81bce2c2511192a6c7d
jyh-centos9 <568000273@qq.com>
2024-11-22 19:27:21 +0800

    complete distributed peers

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"distributed-cache-demo/jincache"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,8 +14,8 @@ var db = map[string]string{
 	"Sam":  "567",
 }
 
-func main() {
-	jincache.NewGroup("scores", 2<<10, jincache.GetterFunc(
+func createGroup() *jincache.Group {
+	return jincache.NewGroup("scores", 2<<10, jincache.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := db[key]; ok {
@@ -22,9 +23,56 @@ func main() {
 			}
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
+}
 
-	addr := "localhost:9999"
+func startCacheServer(addr string, addrs []string, gee *jincache.Group) {
 	peers := jincache.NewHTTPPool(addr)
-	log.Println("geecache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
+	log.Println("jincache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func startAPIServer(apiAddr string, gee *jincache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := gee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+
+}
+
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "jincache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	gee := createGroup()
+	if api {
+		go startAPIServer(apiAddr, gee)
+	}
+	startCacheServer(addrMap[port], []string(addrs), gee)
 }
