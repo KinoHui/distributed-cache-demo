38d89c82cbfbf432f9820c478faac87d03a42ece
jyh-centos9 <568000273@qq.com>
2024-11-24 09:39:52 +0800

    implement singleglight to make sure each key is only fetched once (either locally or remotely) regardless of the number of concurrent callers.

--- main.go
+++ main.go
@@ -25,19 +25,19 @@ func createGroup() *jincache.Group {
 		}))
 }
 
-func startCacheServer(addr string, addrs []string, gee *jincache.Group) {
+func startCacheServer(addr string, addrs []string, jin *jincache.Group) {
 	peers := jincache.NewHTTPPool(addr)
 	peers.Set(addrs...)
-	gee.RegisterPeers(peers)
+	jin.RegisterPeers(peers)
 	log.Println("jincache is running at", addr)
 	log.Fatal(http.ListenAndServe(addr[7:], peers))
 }
 
-func startAPIServer(apiAddr string, gee *jincache.Group) {
+func startAPIServer(apiAddr string, jin *jincache.Group) {
 	http.Handle("/api", http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			key := r.URL.Query().Get("key")
-			view, err := gee.Get(key)
+			view, err := jin.Get(key)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
@@ -70,9 +70,9 @@ func main() {
 		addrs = append(addrs, v)
 	}
 
-	gee := createGroup()
+	jin := createGroup()
 	if api {
-		go startAPIServer(apiAddr, gee)
+		go startAPIServer(apiAddr, jin)
 	}
-	startCacheServer(addrMap[port], []string(addrs), gee)
+	startCacheServer(addrMap[port], []string(addrs), jin)
 }
